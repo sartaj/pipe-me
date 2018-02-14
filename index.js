35e94b11bf055368e307b55b86da291e3f096cfe
Sartaj <sartaj@sartaj.me>
2018-02-14 07:22:13 -0600

    wip(docs): added basic docs test

--- index.js
+++ index.js
@@ -6,37 +6,84 @@ const fromObservable  = require('callbag-from-obs')
 const fromIterable = require('callbag-from-iter')
 const fromEvent = require('callbag-from-event')
 const fromPromise = require('callbag-from-promise')
+const filter = require('callbag-filter')
 
 module.exports = {
   // Create
   // TODO: Make sure everything is multicast
-  fromObservable,
-  fromIterable,
-  fromEvent,
-  fromPromise,
+  fromObservable: (...args) => share(fromObservable(...args)),
+  fromIterable: (...args) => share(fromIterable(...args)),
+  fromEvent: (...args) => share(fromEvent(...args)),
+  fromPromise: (...args) => share(fromPromise(...args)),
   // fromAsync: fromPromise,
 
   // Side Effects
+
+  /**
+   * # Side Effect
+   */
   sideEffect: require('callbag-for-each'),
+
+  /**
+   * # this is a test
+   */
   log: (res) => forEach(((...args) => console.log(...args)))(res),
 
-  // Transform Data
+  /**
+   * # this is a test
+   */
   map: map,
+
+  /**
+   * # this is a test
+   */
   accumulate: require('callbag-scan'),
+
+  /**
+   * # this is a test
+   */
   scan: () => {
     throw new Error('If you meant `scan` in the Reactive Stream sense, Please use `accumulate` instead.')
   },
+
+  /**
+   * # this is a test
+   */
   flatten: require('callbag-flatten'),
 
-  // Filters
+  /**
+   * # this is a test
+   */
   take: require('callbag-take'),
-  skip: require('callbag-skip'),
-  filter: require('callbag-filter'),
 
-  // Combiners
+  /**
+   * # this is a test
+   */
+  skip: require('callbag-skip'),
+
+  /**
+   * # this is a test
+   */
+  filter,
+
+  /**
+   * # this is a test
+   */
   // switchTo: res => map(() => res),
+
+  /**
+   * # this is a test
+   */
   merge: require('callbag-merge'),
+
+  /**
+   * # this is a test
+   */
   concat: require('callbag-concat'),
+
+  /**
+   * # this is a test
+   */
   combine: require('callbag-combine')
 }
 
