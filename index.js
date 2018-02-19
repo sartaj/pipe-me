f8d7b8d4abef040bac59b32e67f3dfb96bb944db
Sartaj <sartaj@sartaj.me>
2018-02-19 10:06:52 -0600

    add basic annotations to all

--- index.js
+++ index.js
@@ -9,113 +9,155 @@ const fromPromise = require('callbag-from-promise')
 const filter = require('callbag-filter')
 
 /**
- *  # THIS IS MY MODULE
+ *  # `pipe-me`
 */
 module.exports = {
   // Create
+  /**
+    # fromObservable
+  */
   fromObservable: (...args) => share(fromObservable(...args)),
-  fromIterable: (...args) => share(fromIterable(...args)),
-  fromEvent: (...args) => share(fromEvent(...args)),
-  fromPromise: (...args) => share(fromPromise(...args)),
-  // fromAsync: fromPromise,
-
-  // Side Effects
 
   /**
-   * # `sideEffect`
-   * Side effects are reactions to your data flow. Things like actual
-   * user interface code (`<div></div>`), the browser url bar, network requests,
-   * LED lights turning on, etc. Create any side effect here.
-   * 
-   * ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/sideEffect.png?raw=true)
-   *
+    # fromObservable
+  */
+  fromIterable: (...args) => share(fromIterable(...args)),
+
+  /**
+    # fromObservable
+  */
+  fromEvent: (...args) => share(fromEvent(...args)),
+
+  /**
+    # fromPromise
+  */
+  fromPromise: (...args) => share(fromPromise(...args)),
+
+  /**
+    # `sideEffect`
+    Side effects are reactions to your data flow. Things like actual
+    user interface code (`<div></div>`), the browser url bar, network requests,
+    LED lights turning on, etc. Create any side effect here.
+    
+    ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/sideEffect.png?raw=true)
    */
   sideEffect: require('callbag-for-each'),
 
   /**
-   * # `log`
-   * Shortcut for sideEffect((data) => { console.log(data) })
+    # `log`
+    Shortcut for sideEffect((data) => { console.log(data) })
    */
   log: (res) => forEach(((...args) => console.log(...args)))(res),
 
   /**
-   * # `map`
-   * ### ***(aka transforming streams)***
-   *
-   * Whenever you want to transform/change the contents of your stream, you'll use map.
-   * 
-   * ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
-   * 
+    # `map`
+    ### ***(aka transforming streams)***
+   
+    Whenever you want to transform/change the contents of your stream, you'll use map.
+    
+    ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
+    
    */
   map: map,
 
   /**
-   * # `accumulate`
-   * aka(ish): Rx: 'scan' | Arrays: 'reduce'
-   *
-   * This allows you to build up data over time.
-   * ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
+    # `accumulate`
+    aka(ish): Rx: 'scan' | Arrays: 'reduce'
+   
+    This allows you to build up data over time.
+    ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
    */
   accumulate: require('callbag-scan'),
 
   /**
-   * # `flatten`
-   * 
-   * ![](https://raw.githubusercontent.com/sartaj/pipe-me/feature/inline-docs/docs/assets/memes/stream-within-a-stream.jpg)
-   * 
-   * Sometimes, the contents of your callbag stream may be another stream.
-   * 
-   * ```js
-   *  const fetchData = fromPromise(callAPI)
-   *
-   *  // This has fetchData Stream itself, not it's contents.
-   *  const dataStreamRetrieved = userClicked.map(fetchData)
-   * ```
-   *
-   * `flatten` can make that data the main stream.
-   * 
-   * ```
-   *  // This actually has the data.
-   *  const dataRetrieved = flatten(dataStreamRetrieved)
-   * ```
-   * 
-   * `|> map() |> flatten` is another way to do 'switchTo' in this library, also known as `flatMap` and `switchMap`.
-   * 
-   */
+    # `flatten`
+    
+    ![](https://raw.githubusercontent.com/sartaj/pipe-me/feature/inline-docs/docs/assets/memes/stream-in-stream.jpg)
+    
+    Sometimes, the contents of your callbag stream may be another stream.
+    
+    ```js
+     const fetchData = fromPromise(callAPI)
+   
+     // This has fetchData Stream itself, not it's contents.
+     const dataStreamRetrieved = userClicked.map(fetchData)
+    ```
+   
+    `flatten` can make that data the main stream.
+    
+    ```
+     // This actually has the data.
+     const dataRetrieved = flatten(dataStreamRetrieved)
+    ```
+    
+    ## AKA
+   
+    ### Observables
+    `|> map() |> flatten` is another way to do 'switchTo' in this library, also known as `flatMap` and `switchMap`.
+    
+    ### Arrays
+    `Array.flatten` will flatten arrays within arrays.
+  */
   flatten: require('callbag-flatten'),
 
   /**
-   * # this is a test
+    # `flatten`
+    
+    ![](https://raw.githubusercontent.com/sartaj/pipe-me/feature/inline-docs/docs/assets/memes/stream-in-stream.jpg)
+    
+    Sometimes, the contents of your callbag stream may be another stream.
+    
+    ```js
+     const fetchData = fromPromise(callAPI)
+   
+     // This has fetchData Stream itself, not it's contents.
+     const dataStreamRetrieved = userClicked.map(fetchData)
+    ```
+   
+    `flatten` can make that data the main stream.
+    
+    ```
+     // This actually has the data.
+     const dataRetrieved = flatten(dataStreamRetrieved)
+    ```
+    
+    ## AKA
+   
+    ### Observables
+    `|> map() |> flatten` is another way to do 'switchTo' in this library, also known as `flatMap` and `switchMap`.
+    
+    ### Arrays
+    `Array.flatten` will flatten arrays within arrays.
    */
   take: require('callbag-take'),
 
   /**
-   * # this is a test
+    # skip
    */
   skip: require('callbag-skip'),
 
   /**
-   * # this is a test
+    # filter
    */
   filter,
 
-  /**
-   * # this is a test
-   */
-  // switchTo: res => map(() => res),
+  // /**
+  //   # switchTo
+  //  */
+  // switchTo: res => map(() => res)(flatten),
 
   /**
-   * # this is a test
+    # merge
    */
   merge: require('callbag-merge'),
 
   /**
-   * # this is a test
+    # concat
    */
   concat: require('callbag-concat'),
 
   /**
-   * # this is a test
+    # combine
    */
   combine: require('callbag-combine')
 }
