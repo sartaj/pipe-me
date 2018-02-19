8670239fa27cee4af49272de09019f53400a25e6
Sartaj <sartaj@sartaj.me>
2018-02-19 15:18:04 -0600

    wip: all docs

--- index.js
+++ index.js
@@ -6,159 +6,234 @@ const fromObservable  = require('callbag-from-obs')
 const fromIterable = require('callbag-from-iter')
 const fromEvent = require('callbag-from-event')
 const fromPromise = require('callbag-from-promise')
-const filter = require('callbag-filter')
 
 /**
- *  # `pipe-me`
-*/
+ *  pipe-me
+ *  --------------
+ */
 module.exports = {
-  // Create
+
   /**
-    # fromObservable
-  */
+   * fromObservable
+   * --------------
+   * 
+   * Convert from Observables in Rx.js, xstream, or anything with a `subscribe` method.
+   * 
+   * ```js
+   *     import { Observable } from 'rxjs'
+   *     import { fromObservable } from 'pipe-me/create'
+   * 
+   *     const observed = fromObservable(Observable.of([0, 1, 2])
+   * ```
+   */
   fromObservable: (...args) => share(fromObservable(...args)),
 
   /**
-    # fromObservable
-  */
+   * fromIterable
+   * --------------
+   * 
+   * aka(ish): fromArray & fromGeneratorFunction
+   * 
+   * Create from Iterable things like generators and arrays
+   * ```js
+   *    import { Observable } from 'rxjs'
+   *    import { fromIterable } from 'pipe-me/create'
+   * 
+   *    function* generate(i) {
+   *       yield i*2;
+   *    }
+   *    const myArray = [0, 1, 2]
+   *    const fromGenerator = fromIterable(generate(2)) // 4
+   * 
+   *    const fromArray = fromIterable(myArray) // 0
+   *                                            // 1
+   *                                            // 2
+   * ```
+   */
   fromIterable: (...args) => share(fromIterable(...args)),
 
   /**
-    # fromObservable
-  */
+   * fromEvent
+   * --------------
+   * 
+   * Get data from any event listener.
+   * 
+   * ```js
+   *     import { fromEvent } from 'pipe-me/create'
+   *     const buttonClicked = fromEvent(document, 'click')
+   *         |> filter(event => event.target.tagName === 'BUTTON')
+   * ```
+   */
   fromEvent: (...args) => share(fromEvent(...args)),
 
   /**
-    # fromPromise
-  */
+   * fromPromise
+   * --------------
+   * 
+   * Create stream from a Promise.
+   * 
+   * ```js
+   *     import { fromPromise } from 'pipe-me/create'
+   * 
+   *     const promiseResolved = fromPromise(Promise.resolve([0, 1, 2])
+   * ```
+   */
   fromPromise: (...args) => share(fromPromise(...args)),
 
   /**
-    # `sideEffect`
-    Side effects are reactions to your data flow. Things like actual
-    user interface code (`<div></div>`), the browser url bar, network requests,
-    LED lights turning on, etc. Create any side effect here.
-    
-    ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/sideEffect.png?raw=true)
+   * sideEffect
+   * --------------
+   * 
+   * Side effects are reactions to your data flow. Things like actual
+   * user interface code (`<div></div>`), the browser url bar, network requests,
+   * LED lights turning on, etc. Create any side effect here.
+   *
+   * ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/sideEffect.png?raw=true)
    */
   sideEffect: require('callbag-for-each'),
 
   /**
-    # `log`
-    Shortcut for sideEffect((data) => { console.log(data) })
+   * log
+   * --------------
+   * 
+   * Shortcut for sideEffect((data) => { console.log(data) })
    */
   log: (res) => forEach(((...args) => console.log(...args)))(res),
 
   /**
-    # `map`
-    ### ***(aka transforming streams)***
-   
-    Whenever you want to transform/change the contents of your stream, you'll use map.
-    
-    ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
-    
+   * map
+   * --------------
+   * 
+   * Whenever you want to transform/change the contents of your stream, you'll use map.
+   *
+   * ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
+   *
+   * ***(aka transforming streams)***
    */
   map: map,
 
   /**
-    # `accumulate`
-    aka(ish): Rx: 'scan' | Arrays: 'reduce'
-   
-    This allows you to build up data over time.
-    ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
+   * accumulate
+   * --------------
+   * 
+   * aka(ish): Rx: 'scan' | Arrays: 'reduce'
+   * 
+   * This allows you to build up data over time.
+   * ![Diagram for map](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/map.png?raw=true)
    */
   accumulate: require('callbag-scan'),
 
   /**
-    # `flatten`
-    
-    ![](https://raw.githubusercontent.com/sartaj/pipe-me/feature/inline-docs/docs/assets/memes/stream-in-stream.jpg)
-    
-    Sometimes, the contents of your callbag stream may be another stream.
-    
-    ```js
-     const fetchData = fromPromise(callAPI)
-   
-     // This has fetchData Stream itself, not it's contents.
-     const dataStreamRetrieved = userClicked.map(fetchData)
-    ```
-   
-    `flatten` can make that data the main stream.
-    
-    ```
-     // This actually has the data.
-     const dataRetrieved = flatten(dataStreamRetrieved)
-    ```
-    
-    ## AKA
-   
-    ### Observables
-    `|> map() |> flatten` is another way to do 'switchTo' in this library, also known as `flatMap` and `switchMap`.
-    
-    ### Arrays
-    `Array.flatten` will flatten arrays within arrays.
-  */
+   * flatten
+   * --------------
+   * 
+   * Sometimes, the contents of source may be another stream. As in, a stream within a stream.
+   * 
+   * ![](https://raw.githubusercontent.com/sartaj/pipe-me/feature/inline-docs/docs/assets/memes/stream-in-stream.jpg)
+   * 
+   * `flatten` can make that data the main stream.
+   * 
+   * ## Example
+   * 
+   * ```javascript
+   *     const fetchData = fromPromise(callAPI)
+   * 
+   *     // This has fetchData Stream itself, not it's contents.
+   *     const dataStreamRetrieved = userClicked.map(fetchData)
+   * 
+   *     // This actually has the data.
+   *     const dataRetrieved = flatten(dataStreamRetrieved)
+   * ```
+   * 
+   * ## In Other Worlds
+   * 
+   * ### Observables
+   * `|> map() |> flatten` is another way to do `flatMap`/`switchMap`.
+   * 
+   * ### Arrays
+   * `Array.flatten` will flatten arrays within arrays.
+   */
   flatten: require('callbag-flatten'),
 
   /**
-    # `flatten`
-    
-    ![](https://raw.githubusercontent.com/sartaj/pipe-me/feature/inline-docs/docs/assets/memes/stream-in-stream.jpg)
-    
-    Sometimes, the contents of your callbag stream may be another stream.
-    
-    ```js
-     const fetchData = fromPromise(callAPI)
-   
-     // This has fetchData Stream itself, not it's contents.
-     const dataStreamRetrieved = userClicked.map(fetchData)
-    ```
-   
-    `flatten` can make that data the main stream.
-    
-    ```
-     // This actually has the data.
-     const dataRetrieved = flatten(dataStreamRetrieved)
-    ```
-    
-    ## AKA
-   
-    ### Observables
-    `|> map() |> flatten` is another way to do 'switchTo' in this library, also known as `flatMap` and `switchMap`.
-    
-    ### Arrays
-    `Array.flatten` will flatten arrays within arrays.
+   * take
+   * --------------
+   *
+   * A filter to take only what you need.
+   * 
+   * ```javascript
+   *      // Will take only first 5 clicks
+   *      const userClicked = listenToClick() |> take(5)
+   * ```
    */
   take: require('callbag-take'),
 
   /**
-    # skip
+   * skip
+   * --------------
+   * 
+   * Ignores the first n of the source.
+   * 
+   * ```javascript
+   *     // Ignore first 5 clicks
+   *     const userClicked = listenToClick() |> skip(5)
+   * ```
    */
   skip: require('callbag-skip'),
 
   /**
-    # filter
+   * filter
+   * --------------
+   *
+   * Filter data based on truthy/falsy condition
+   * 
+   * ```javascript
+   *    fromIterable([1,2,3,4,5])
+   *       |> filter(x => x % 2);
+   *       |> log // 1
+   *              // 3
+   *              // 5
+   * ```
    */
-  filter,
-
-  // /**
-  //   # switchTo
-  //  */
-  // switchTo: res => map(() => res)(flatten),
+  filter: require('callbag-filter'),
 
   /**
-    # merge
+   * merge
+   * --------------
+   * 
+   * Merges data from multiple callbag sources. Designed more for listening sources (like fromEvent)
+   * 
+   *     const userWantsToSubmit = merge(userClickedSubmit, userPressedEnter);
+   * 
+   *     userWantsToSubmit |> log // click event
+   *                              // keyboard enter event
+   *                              // keyboard enter event
+   *                              // click event
+   *                              // etc...
    */
   merge: require('callbag-merge'),
-
+  
   /**
-    # concat
+   * concat
+   * --------------
+   * 
+   * Combine sources by putting one source after the previous source ends..
+   * 
+   * ![Diagram for concat](https://github.com/sartaj/pipe-me/blob/feature/inline-docs/docs/assets/diagrams/concat.png?raw=true)
    */
   concat: require('callbag-concat'),
 
   /**
-    # combine
-   */
+   * combine
+   * ---------------
+   *
+   * Get the latest value from multiple streams as an array.
+   * 
+   * ```javascript
+   *     const state = combine(uiStream, serverStream, userStream);
+   *     state |> log // [uiState, serverState, userStream]
+   * ```
+   */ 
   combine: require('callbag-combine')
 }
 
